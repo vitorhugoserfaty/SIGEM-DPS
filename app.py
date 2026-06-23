e06755990dce79f08d95fb01a92f0280bb795f28
vitorhugoserfaty <vitorhugoserfaty@gmail.com>
2026-06-23 09:48:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def login():
-    return "SIGEM-DPS iniciado com sucesso!"
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
